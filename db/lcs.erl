5514bd89fa5866158eb5dc606ac3ebde98254c30
Pratap Chakravarthy <prataprc@gmail.com>
2012-02-01 11:40:11 +0530

    Test data for lcs.

--- db/lcs.erl
+++ db/lcs.erl
@@ -1,28 +1,36 @@
 -module(lcs).
--export([ lcs/2 ]).
+-export([ lcs/2, main/1 ]).
 
-nfold( _, A0, [],     _ ) -> A0;
-nfold( F, A0, [E|Es], N ) -> nfold(F, F(A0, E, N), Es, N+1).
+max( L1, L2 ) when length(L1) > length(L2) -> L1;
+max( _, L2 ) -> L2.
 
-nfold( F, A, L ) -> nfold(F, A, L, 0).
+%% Reduce an array by passing offset N to the folding function.
+reduce( _, A0, [], _ ) ->
+    A0;
+reduce( F, A0, [E|Es], N ) ->
+    reduce(F, F(A0, E, N), Es, N+1).
+
+reduce( F, A, L ) -> reduce( F, A, L, 0 ).
 
 
-lcsfold( A0, Rs, Cs, CSize ) ->
+%% Reduce a 2-dimensional array of N-Rows and M-Columns by computing 
+%% Longest Common Subsequence (LCS).
+rcfold( A0, Rs, Cs, CSize ) ->
     Get = fun (A, I, J   ) -> array:get( I*CSize+J, A ) end,
     Set = fun (A, I, J, V) -> array:set( I*CSize+J, V, A ) end,
-    nfold(
+    reduce(
       fun
-      (A0, R, I) ->
-		 nfold(
+      (AR, R, I) ->
+		 reduce(
 		   fun
-		   (A0, C, J) when R =:= C ->
-			      L = [R | Get(I, J, A0)],
-			      Set( I+1, J+1, L, A0 );
-		   (A0, _, J) ->
-			      L = max( Get(I+1, J, A0), Get(I, J+1, A0) ),
-			      Set(I+1, J+1, L, A0)
+		   (AC, C, J) when R =:= C ->
+			      V = [R | Get(AC, I, J)],
+			      Set( AC, I+1, J+1, V );
+		   (AC, _, J) ->
+			      V = max( Get(AC, I+1, J), Get(AC, I, J+1) ),
+			      Set( AC, I+1, J+1, V )
 		   end,
-		   A0,
+		   AR,
 		   Cs )
       end,
       A0,
@@ -33,63 +41,41 @@ lcs( Rs, Cs ) ->
     {Rlen, Clen} = {length(Rs), length(Cs)},
     ASize = (Rlen+1) * (Clen+1),
     A0 = array:new( ASize, [ {fixed, true}, {default, []} ] ),
-    A1 = lcsfold( A0, Rs, Cs, Clen+1 ),
+    A1 = rcfold( A0, Rs, Cs, Clen+1 ),
     lists:reverse( array:get( Rlen*(Clen+1)+Clen, A1 )).
 
 
-edits( [C|Lcs]=LCS, [X|Xs]=XXs, [Y|Ys]=YYs, Acc ) >
+edits( [], [],     [],     Acc ) ->
+    lists:reverse(Acc);
+edits( [C|Lcs]=LCS, [X|Xs]=XXs, [Y|Ys]=YYs, Acc ) ->
     if 
         X =/= C -> edits( LCS, Xs,  YYs, [{d, X} | Acc] );
         Y =/= C -> edits( LCS, XXs, Ys,  [{a, Y} | Acc] );
         true    -> edits( Lcs, Xs,  Ys,  [{e, C} | Acc] )
     end;
-edits( [], [X|Xs], Ys,     Acc ) -> edits( [], Xs, Ys, [{d, X} | Acc]);
-edits( [], [],     [Y|Ys], Acc ) -> edits( [], [], Ys, [{a, Y} | Acc]);
-edits( [], [],     [],     Acc ) -> lists:reverse(Acc).
+edits( [], [X|Xs], Ys,     Acc ) ->
+    edits( [], Xs, Ys, [{d, X} | Acc]);
+edits( [], [],     [Y|Ys], Acc ) ->
+    edits( [], [], Ys, [{a, Y} | Acc]).
 
 edits( Xs, Ys ) -> edits( lcs(Xs, Ys), Xs, Ys, [] ).
 
 
-Rangeof( I, I ) -> io_lib:format( "~w", [I] );
-Rangeof( I, J ) -> io_lib:format( "~w,J", [I,J] );
+%% Test code
 
+linesof( <<>>, [<<>> | Ls] ) ->
+    lists:reverse(Ls);
+linesof( <<>>, Ls ) ->
+    lists:reverse(Ls);
+linesof( <<B:8,Tail/binary>>, [L|Ls] ) ->
+    if 
+        B =:= $\n -> linesof( Tail, [ <<>>, <<L/binary,B:8>> | Ls ]);
+        true           -> linesof( Tail, [ <<L/binary,B:8>> | Ls ] )
+    end.
 
-hunk( Xri, Xrj, Yri, Yrj, Cmd, Hunk, Hunks ) ->
-    Hdr = io_lib:format( "~w~w~w~", [ Rangeof(Xri,Xrj), Cmd, Rangeof(Yri,Yrj) ]),
-    Hunk_ = [ Hdr | lists:reverse( Hunk ) ],
-    hunkify( Xi+1, Yj+1, Es, [ Hunk_ | Hunks ] ).
-
-
-%% end-of-edit
-hunkify( Xi, Yj, [], {Xri, Xrj}=Xr, Cmd, {Yri, Yrj}=Yr, Hunk, Hunks ) ->
-    hunk( Xri, Xrj, Yri, Yrj, Cmd, Hunk, Hunks );
-%% Delete-hunk, continue deleting
-hunkify( Xi, Yj, [{d,Ch}|Es], {Xri, Xrj}=Xr, d, Yr, Hunk, Hunks ) ->
-    hunkify( Xi+1, Yj, Es, {Xri, Xi}, d, Yr, ["< "++[Ch] | Hunk], Hunks ] );
-%% Delete-hunk, end of delete-hunk
-hunkify( Xi, Yj, [{e,Ch}|Es], {Xri, Xrj}=Xr, d, {Yri, Yrj}=Yr, Hunk, Hunks ) ->
-    hunkify( Xi+1, Yj+1, Es, hunk(Xri, Xrj, Yri, Yrj, d, Hunk, Hunks) );
-%% Delete-hunk, change-over
-hunkify( Xi, Yj, [{a,Ch}|Es], Xr, d, {Yri, Yrj}=Yr, Hunk, Hunks ) ->
-    Hunk_ = [ "> "++[Ch], "----" ] ++ Hunk,
-    hunkify( Xi, Yj+1, Es, Xr, a, {Yri,Yj}, Hunk_, Hunks ] );
-%% Add-hunk, continue adding
-hunkify( Xi, Yj, [{a,Ch}|Es], Xr, a, {Yri, Yrj}=Yr, Hunk, Hunks ) ->
-    hunkify( Xi, Yj+1, Es, Xr, a, {Yri,Yj}, ["> "++[Ch] | Hunk], Hunks ] );
-%% Add-hunk, end of add-hunk
-hunkify( Xi, Yj, [{e,Ch}|Es], {Xri, Xrj}=Xr, a, {Yri, Yrj}=Yr, Hunk, Hunks ) ->
-    hunkify( Xi+1, Yj+1, Es, hunk(Xri, Xrj, Yri, Yrj, a, Hunk, Hunks) );
-%% Add-hunk, change-over
-hunkify( Xi, Yj, [{d,Ch}|Es], {Xri, Xrj}=Xr, a, Yr, Hunk, Hunks ) ->
-    Hunk_ = [ "< "++[Ch], "----" ] ++ Hunk,
-    hunkify( Xi+1, Yj, Es, {Xri, Xi}, a, Yr, Hunk_, Hunks ] );
-
-%% Make edit hunks
-hunkify( Xi, Yj, [{e,_}|Es], Hunks ) ->
-    Hunks
-hunkify( Xi, Yj, [{e,_}|Es], Hunks ) ->
-    hunkify( Xi+1, Yj+1, Es, Hunks );
-hunkify( Xi, Yj, [{d,Ch}|Es], Hunks ) ->    % Start a delete-hunk
-    hunkify( Xi+1, Yj, Es, {Xi,Xi}, d, {Yj,Yj}, [ "< "++[Ch] ], [] );
-hunkify( Xi, Yj, [{a,Ch}|Es], Hunks ) ->    % Start a add-hunk
-    hunkify( Xi, Yj+1, Es, {Xi,Xi}, a, {Yj,Yj}, [ "> "++[Ch] ], [] );
+main([ File1, File2 ]) ->
+    {ok, Text1} = file:read_file( File1 ),
+    {ok, Text2} = file:read_file( File2 ),
+    Lines1 = linesof( Text1, [<<>>] ),
+    Lines2 = linesof( Text2, [<<>>] ),
+    io:format("~p ~n", [ edits(Lines1, Lines2) ] ).
--- db/store.hrl
+++ db/store.hrl
@@ -1,39 +1,55 @@
-%% `changeset` record is a list of hunks that consititute a patch.
-%%  A hunk can be - chgval, addprops, delprops, chgprop
+%% Commutable patches :
+%%      A patch is a collection of commutable changesets, where each changeset 
+%% pertains to a single document and all changesets in a patch pertaining to
+%% several documents are to be atomically applied to the database. If one 
+%% changeset fails entire patch fails to apply.
+%%      A patch is considered commutable only if all changesets inside the
+%% patch is commutable with respect to previous patch. 
+%% If a patch is non-commutable with respect to previous patch, then both
+%% patches are deemed as dependants and 
 %%
-%%  Every changeset is identified by a unique hash generated from
-%%  its hunks.
--record( changeset, { id, hunks=[] }).
+%% A changeset can be one of the following,
+%%      hunk, adoc, rdoc
+-record( patch,     { sha, backref, context, changesets=[], effect }).
 
-%% `chgval` tuple is a hunk for changes done to simple term values like
-%% string, number, true, false, null inside a more complex terms like list and
-%% property list.
--record( chgval,    { old, new }).
+%% `hunk` record is a list of diffs that constitute a changeset between two
+%%  versions of a document.
+%%  A hunk can be,
+%%      cv, ap, dp, np, al, dl, cl, nl
+%%  Every hunk is identified by unique document id and hash generated from its
+%%  diffs.
+-record( hunk,      { uid, sha, backref, diffs=[] }).
+%% Every document is identified by a unique id. Along the way, if user wishes
+%% to change the unique id, it can be done so using `rdoc` changeset
+-record( adoc,      { uid }).
+-record( rdoc,      { uid, backref }).
 
-%% `addprops` tuple is a hunk of property additions.
-%% `from`, `to` specify the start and end position inside the eventual 
-%% document-state. `ins` is a list of `diffprop` records.
--record( addprops,  { from, to, ins=[] }).
+%% `cv` tuple is diff of changes done to simple term values like,
+%%      string, number, true, false, null
+%% inside a more complex terms like list and property list.
+-record( cv,        { old, new }).
 
-%% `delprops` tuple is a hunk of property deletions.
-%% `from`, `to` specify the start and end position inside the present
-%% document-state. `outs` is a list of `diffprop` records.
--record( delprops,  { from, to, outs=[] }).
+%% `ap` tuple is `diffprop` of property additions. {from,to} specify the start 
+%% and end position inside the eventual document-state.
+-record( ap,        { from, to, ins=[] }).
 
-%% `chgprop` tuple is a hunk of single property change in property list. 
-%% `offthis` and `offthat` specify the position of property inside current
-%% document-state and eventual document-state.
--record( chgprop,   { offthis, offthat, changeset }).
+%% `dp` tuple is `diffprop` of property deletions. {from,to} specify the start
+%% and end position inside the present document-state.
+-record( dp,        { from, to, outs=[] }).
 
-%% `addems` tuple is a hunk of elements to add inside a list.
-%% `from` and `to` specify the start and end position inside the eventual
-%% document-state.
--record( addems,    { from, to, ins=[] }).
+%% `np` tuple is diff of single property change in property list. 
+%% {offthis,offthat} specify the position of property inside current
+%% document-state and eventual document-state, respectively.
+-record( np,        { offthis, offthat, changeset }).
 
-%% `delems` tuple is a hunk of elements to delete from a list.
+%% `ae` tuple is diff of elements to add inside a list. {from,to} specify 
+%% the start and end position inside the eventual document-state.
+-record( ae,        { from, to, ins=[] }).
+
+%% `delems` tuple is diff of elements to delete from a list.
 %% `from` and `to` specify the start and end position inside the present
 %% document-state.
--record( delems,    { from, to, outs=[] }).
+-record( de,        { from, to, outs=[] }).
 
 %% `chgems` tuple is a hunk of elements to replace from present document-state,
 %% with new set of elements leading to an eventual document-state.
